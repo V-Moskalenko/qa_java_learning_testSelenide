617082ae4e5758208ce538f8f606ff8d3402af1a
V-Moskalenko <mymewtwo@gmail.com>
2022-06-01 15:28:57 +0300

    Добавлены properties

--- src/test/java/TestSelenide.java
+++ src/test/java/TestSelenide.java
@@ -1,4 +1,5 @@
 import Hooks.WebHooks;
+import Utils.Configuration;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.DisplayName;
@@ -14,23 +15,23 @@ public class TestSelenide extends WebHooks {
     @Test
     @DisplayName("Тест авторизации")
     public void Test_1(){
-        open("https://edujira.ifellow.ru");
-        authStep("vmoskalenko", "123Qwerty");
+        open(Configuration.getValue("URL"));
+        authStep(Configuration.getValue("loginName"), Configuration.getValue("passwordName"));
     }
 
     @Test
     @DisplayName("Проверка заведённых задач")
     public void Test_2(){
-        open("https://edujira.ifellow.ru");
-        authStep("vmoskalenko", "123Qwerty");
+        open(Configuration.getValue("URL"));
+        authStep(Configuration.getValue("loginName"), Configuration.getValue("passwordName"));
         getCountTask();
     }
 
     @Test
     @DisplayName("Проверка статуса и версии задачи")
     public void Test_3(){
-        open("https://edujira.ifellow.ru");
-        authStep("vmoskalenko", "123Qwerty");
+        open(Configuration.getValue("URL"));
+        authStep(Configuration.getValue("loginName"), Configuration.getValue("passwordName"));
         getCountTask();
         testTask();
     }
@@ -38,8 +39,8 @@ public class TestSelenide extends WebHooks {
     @Test
     @DisplayName("Заведение новой задачи")
     public void Test_4() {
-        open("https://edujira.ifellow.ru");
-        authStep("vmoskalenko", "123Qwerty");
+        open(Configuration.getValue("URL"));
+        authStep(Configuration.getValue("loginName"), Configuration.getValue("passwordName"));
         getCountTask();
         testTask();
         createTask("TEST-05052022", "CreateTest002");
